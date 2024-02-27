58f04aa489ec42634b63f74f082fda5b75e510f9
JoaoStahlke <stahlke09@gmail.com>
2024-02-27 09:52:37 -0300

    Abertura da Base do Código

--- PJBL1.java
+++ PJBL1.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class PJBL1 {
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // Criação de Objeto Scanner (Scan) para ler inputs dos usuários.
+
+        System.out.print("Digite o Primeiro Número: "); // Entrada de Números pelo usuário e gravados na Double (variáveis).
+        double num1 = scan.nextDouble();
+
+        System.out.print("Digite o Segundo Número: ");
+        double num2 = scan.nextDouble();
+
+        System.out.print("Escolha a Operação (+, -, *, /): "); // Input de Operação pelo usuário.
+        char operacao = scan.next().charAt(0); // Criação de Variável "operacao" que recebe um caracter de operação. Depois le a posição 0, onde esta o caracter da operação.
+
+        double result = 0;
+        switch (operacao) {
+            case '+': // Abertura de "case" para as diferentes operações
+                result = num1 + num2;
+                break;
+
+
+        }
