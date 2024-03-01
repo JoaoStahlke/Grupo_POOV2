793a2c6ec04b6f1eded3128136e8e73622394a81
JoaoStahlke <stahlke09@gmail.com>
2024-03-01 18:53:29 -0300

    Create PBL2

---
+++ PBL2.java
@@ -0,0 +1,14 @@
+// Implemente um programa em Java que utilize vetores para armazenar informações sobre os
+//jogadores, suas pontuações e a quantidade de moedas que coletaram em cada nível.
+
+import java.util.Scanner;
+public class PBL2 {
+    public static void main(String[] args) {
+        
+
+
+
+
+
+    }
+}
