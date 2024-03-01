d642f0c1f49bf5fedf701fbedf7ea09c176432ee
JoaoStahlke <stahlke09@gmail.com>
2024-03-01 18:59:24 -0300

    Update PBL2.java

--- PBL2.java
+++ PBL2.java
@@ -1,10 +1,12 @@
 // Implemente um programa em Java que utilize vetores para armazenar informações sobre os
 //jogadores, suas pontuações e a quantidade de moedas que coletaram em cada nível.
 
+// Lista de fases, ai dentro dessa temos o Melhor de cada fase.
+
 import java.util.Scanner;
 public class PBL2 {
     public static void main(String[] args) {
-        
+
 
 
 
